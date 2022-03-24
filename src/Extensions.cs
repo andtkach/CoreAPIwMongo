fdec9235fcf81e99b892525b2ea0af384560f151
Andrii Tkach <andtkach@gmail.com>
2022-03-24 15:03:27 +0200

    Init commit

---
+++ src/Extensions.cs
@@ -0,0 +1,13 @@
+using DataService.Dtos;
+using DataService.Entities;
+
+namespace DataService
+{
+    public static class Extensions
+    {
+        public static ItemDto AsDto(this Item item)
+        {
+            return new ItemDto(item.Id, item.Name, item.Description, item.Price, item.CreatedDate);
+        }
+    }
+}
---
+++ src/Settings/ServiceSettings.cs
@@ -0,0 +1,7 @@
+namespace DataService.Settings
+{
+    public class ServiceSettings
+    {
+        public string ServiceName { get; init; }
+    }
+}
